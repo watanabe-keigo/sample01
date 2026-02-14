dd612042551161b8061e40ffa697bbbc1cfd551c
渡部景伍 <kwjun519@gmail.com>
2026-02-14 14:00:42 +0900

    reset, commonのcssを取り込み

--- front-page.php
+++ front-page.php
@@ -5,6 +5,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="robots" content="noindex, nofollow">
 	<title>サンプル01</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100..900&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/common/reset.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/common/common.css">
 	<?php wp_head(); ?>
 </head>
 <body>
