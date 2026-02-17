2af52a58fb9b248257e78bc3952a078074c391ae
渡部景伍 <kwjun519@gmail.com>
2026-02-18 08:38:16 +0900

    メニュー一覧作成

---
+++ archive-menu.php
@@ -0,0 +1,75 @@
+<?php get_header(); ?>
+<!-- css -->
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/menu/css/style.css?<?php echo time(); ?>">
+
+<!-- ファーストビュー -->
+<?php include get_template_directory() . '/contents/firstview/firstview.php'; ?>
+
+<div class="container">
+	<?php include get_template_directory() . '/contents/breadcrumbs/breadcrumbs.php'; ?>
+
+	<div class="menu content_width">
+		<div class="menu_item">
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri(); ?>/common/images/menu_coffee.png" alt="">
+			</a>
+		</div>
+		<div class="menu_item">
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri(); ?>/common/images/menu_coffee.png" alt="">
+			</a>
+		</div>
+		<div class="menu_item">
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri(); ?>/common/images/menu_coffee.png" alt="">
+			</a>
+		</div>
+		<div class="menu_item">
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri(); ?>/common/images/menu_coffee.png" alt="">
+			</a>
+		</div>
+		<div class="menu_item">
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri(); ?>/common/images/menu_coffee.png" alt="">
+			</a>
+		</div>
+		<div class="menu_item">
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri(); ?>/common/images/menu_coffee.png" alt="">
+			</a>
+		</div>
+		<div class="menu_item">
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri(); ?>/common/images/menu_coffee.png" alt="">
+			</a>
+		</div>
+		<div class="menu_item">
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri(); ?>/common/images/menu_coffee.png" alt="">
+			</a>
+		</div>
+		<div class="menu_item">
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri(); ?>/common/images/menu_coffee.png" alt="">
+			</a>
+		</div>
+		<div class="menu_item">
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri(); ?>/common/images/menu_coffee.png" alt="">
+			</a>
+		</div>
+		<div class="menu_item">
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri(); ?>/common/images/menu_coffee.png" alt="">
+			</a>
+		</div>
+		<div class="menu_item">
+			<a href="#">
+				<img src="<?php echo get_template_directory_uri(); ?>/common/images/menu_coffee.png" alt="">
+			</a>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
