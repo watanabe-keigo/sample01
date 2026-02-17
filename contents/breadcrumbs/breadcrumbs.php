b05a3205bc19ec58552c1ad1569c2ffb8d431eb1
渡部景伍 <kwjun519@gmail.com>
2026-02-17 20:47:22 +0900

    パンくずリスト作成

--- contents/breadcrumbs/breadcrumbs.php
+++ contents/breadcrumbs/breadcrumbs.php
@@ -2,12 +2,5 @@
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/contents/breadcrumbs/css/style-breadcrumbs.css?<?php echo time(); ?>">
 
 <div class="breadcrumbs content_width">
-	<ul class="breadcrumbs_list">
-		<li>
-			<a href="#">トップ</a>
-		</li>
-		<li>
-			<a href="#">お知らせ</a>
-		</li>
-	</ul>
+	<?php if (function_exists('bcn_display')) bcn_display(); ?>
 </div>
