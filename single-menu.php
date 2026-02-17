4008951622ccd798d974a5c239548698174f1ee5
渡部景伍 <kwjun519@gmail.com>
2026-02-18 08:26:23 +0900

    メニューページ作成

---
+++ single-menu.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<div class="container">
+	<?php include get_template_directory() . '/contents/breadcrumbs/breadcrumbs.php'; ?>
+	<?php include get_template_directory() . '/contents/article/article.php'; ?>
+</div>
+
+<?php get_footer(); ?>
